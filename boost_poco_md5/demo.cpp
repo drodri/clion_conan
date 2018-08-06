bd0a06de51dc1d255687fdcab09c60816738902f
abhi-jha <abhi_jha1995@rediffmail.com>
2018-08-06 06:22:12 +0530

    stage 1 working

--- boost_poco_md5/demo.cpp
+++ boost_poco_md5/demo.cpp
@@ -1,4 +1,4 @@
-#include "Poco/MD5Engine.h"
+#include <Poco/MD5Engine.h>
 #include <boost/regex.hpp>
 #include <string>
 #include <iostream>
@@ -11,9 +11,14 @@ int main(){
     md5.update("Hello World");
     string md5string = DigestEngine::digestToHex(md5.digest());
     cout<< "MD5= " << md5string << "\n";
-
+    int n = 5;
+    while(n--) {
+        md5.update("hola");
+        cout << "MD5= " << DigestEngine::digestToHex(md5.digest()) << "\n";
+    }
     string s = "correct@email.com", s2="bademail";
     boost::regex expr{"\\b[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}\\b"};
     cout << boolalpha << boost::regex_match(s, expr) << '\n';
     cout << boolalpha << boost::regex_match(s2, expr) << '\n';
+
 }
